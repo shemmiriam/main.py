8cb7343009bdcf7af9908e6dfbca754e9c56ac0b
shem miriam <shemmiriam93@gmail.com>
2022-10-24 12:35:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to the greeter program")
+name = input("Enter your name ")
+print("Greetings: " + name)
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
